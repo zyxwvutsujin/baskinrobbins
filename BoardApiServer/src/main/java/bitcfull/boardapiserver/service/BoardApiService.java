100e36158dc445697f9c2e7126972647cfd593ce
zyxwvutsujin <rhcwnl1904@naver.com>
2024-10-02 08:58:11 +0900

    Delete BoardApiServer directory

--- BoardApiServer/src/main/java/bitcfull/boardapiserver/service/BoardApiService.java
+++
@@ -1,9 +0,0 @@
-package bitcfull.boardapiserver.service;
-
-import bitcfull.boardapiserver.dto.BoardDTO;
-
-import java.util.List;
-
-public interface BoardApiService {
-    List<BoardDTO> selectBoardList();
-}
